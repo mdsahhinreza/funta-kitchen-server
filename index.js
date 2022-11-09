6388d133e1fc03110a48a5899c5e15f766819720
mdsahhinreza <mdsahhinreza@gmail.com>
2022-11-09 17:45:19 +0600

    MongoDB Connection success and Services Api Created Success

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection and API's Here
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.epiqiul.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("funtaKitchen").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
 app.get("/", (req, res) => {
   res.send("Funta-Kitchen server is Running...");
 });
