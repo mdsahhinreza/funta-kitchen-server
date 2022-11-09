0ea1ee95151c5bcbaf7df58d23a1e1151ddf5f27
mdsahhinreza <mdsahhinreza@gmail.com>
2022-11-10 03:10:15 +0600

    Review Delete API added

--- index.js
+++ index.js
@@ -47,6 +47,30 @@ async function run() {
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
+
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          customarEmail: req.query.email,
+        };
+      } else if (req.query.productId) {
+        query = {
+          productId: req.query.productId,
+        };
+      }
+
+      const cursor = reviewCollection.find(query).sort({ reviewTime: -1 });
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
